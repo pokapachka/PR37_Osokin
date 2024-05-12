89f0fc36a6eebf5711137da87a516f93d481ca84
Osokin <pachkaosokin@yandex.ru>
2024-05-12 17:03:17 +0500

    Разработанное подключение к MySql. Осокин

---
+++ Data/Common/Connection.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace ПР37_Осокин.Data.Common
+{
+    public class Connection
+    {
+        readonly static string ConnectionData = "server=localhost;port=3306;database=Shop;uid=root;pwd=;";
+        public static MySqlConnection MySqlOpen()
+        {
+            MySqlConnection NewMySqlConnection = new MySqlConnection(ConnectionData);
+            NewMySqlConnection.Open();
+            return NewMySqlConnection;
+        }
+        public static MySqlDataReader MySqlQuery(string Query, MySqlConnection Connection)
+        {
+            MySqlCommand NewMySqlCommand = new MySqlCommand(Query, Connection);
+            return NewMySqlCommand.ExecuteReader();
+        }
+        public static void MySqlClose(MySqlConnection connection)
+        {
+            connection.Close();
+        }
+    }
+}
