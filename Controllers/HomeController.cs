98b87234c5b90e53b61649b26a03cc0d5bf70558
pokapachka <pachkaosokin@yandex.ru>
2024-05-22 20:58:21 +0500

    Разработанное перенаправление на страницу с предметами. Осокин

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ПР37_Осокин.Controllers
+{
+    public class HomeController : Controller
+    {
+        public RedirectResult Index()
+        {
+            return Redirect("/Items/List");
+        }
+    }
+}
